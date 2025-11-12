bf23a03f4fff39e02576cbe5968315a40c3e5f0a
Joey Sweener <jmsweener@gmail.com>
2025-11-12 14:52:00 -0500

    Delete styles directory
    Preparing to upload a site rewrite to merge onto main...

--- styles/navigation.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("main-nav").classList.add('scrolled');
-  } else {
-    document.getElementById("main-nav").classList.remove('scrolled');
-  }
-}
